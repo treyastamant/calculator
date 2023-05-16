fc21c484a3b38b7674d3b38d897a44a22b6de8de
Treya St Amant <22247165+treyastamant@users.noreply.github.com>
2023-05-15 21:43:31 -0600

    Split display into array. Console log answer!

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
 let num1 = 5;
 let op = "+";
 let num2 = 2;
-let displayValue;
+let displayValue = [];
 
 const display = document.querySelector('#display');
 for (let i = 0; i < 10; i++) {
   const num = document.getElementById(i);
   num.addEventListener('click', () => {
     display.textContent += i;
-  });
-
-  const clear = document.querySelector('#clear');
-  clear.addEventListener('click', () => {
-    display.textContent = "";
+    displayValue.push(i);
+    console.log(displayValue, 'Display Value')
   });
 }
 
@@ -20,17 +17,27 @@ for (let i = 1; i < 5; i++) {
   const oper = document.getElementById('op' + i);
   oper.addEventListener('click', () => {
     let x = oper.textContent;
-    display.textContent += " " + x + " ";
+    display.textContent += ` ${x} `;
+    displayValue.push(x);
   });
 }
 
-
+const clear = document.querySelector('#clear');
+clear.addEventListener('click', () => {
+  display.textContent = "";
+});
 
 const equals = document.querySelector('#op5');
   equals.addEventListener('click', () => {
-    displayValue = display.textContent;
     console.log(displayValue);
-    operate('+', 4, 4);
+    // displayValue.split(' ');
+    console.log(displayValue);
+    num1 = displayValue[0];
+    op = displayValue[1];
+    num2 = displayValue[2];
+    
+    let ans = operate(op, num1, num2);
+    console.log(ans);
   });
 
 
@@ -39,6 +46,13 @@ const equals = document.querySelector('#op5');
 function addIt(a, b) {
   return a + b;
 }
+
+// function addIt(array) {
+//   for (let i = 0; i <= array.length; i++){
+//     let answer = 
+//   }
+//   return a + b;
+// }
 
 function subtractIt(a, b) {
   return a - b;
